755d3b58608003b2c13c2ebf89e929de10c0ecd8
kael <judicael.ratombotiana@gmail.com>
2023-04-18 13:32:31 +0200

    siret first commit

---
+++ src/main/java/com/siret/siret_test/SiretTestApplication.java
@@ -0,0 +1,13 @@
+package com.siret.siret_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiretTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiretTestApplication.class, args);
+    }
+
+}
